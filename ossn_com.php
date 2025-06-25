5cc7ca48eb1a895da9820b2965296404ac181aac
nizone <limanovus@gmail.com>
2025-06-25 23:29:40 +0100

    Make README.md deployable as site

--- ossn_com.php
+++ ossn_com.php
@@ -4,7 +4,7 @@
  * @link      https://www.opensource-socialnetwork.org/
  * @package   Disable Member Self Validating
  * @author    Michael Zülsdorff <ossn@z-mans.net>
- * @copyright (C) Michael Zülsdorff
+ * @copyright (C) staff.ma
  * @license   GNU General Public License https://www.gnu.de/documents/gpl-2.0.en.html
  */
 define('__OSSN_VALDOC__', ossn_route()->com . 'OssnValDoc/');
